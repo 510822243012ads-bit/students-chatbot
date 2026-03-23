21b9ed93671b4c08d22b0e1945e24e97e71c4ef2
510822243012ads-bit <510822243012.ads@gtec.ac.in>
2026-03-23 23:09:35 +0530

    Update app.py

--- student_chatbot/app.py
+++ student_chatbot/app.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Student Performance Bot")
 @st.cache_data
 def load_data():
     # Make sure students.csv is in the same folder as app.py
-    df = pd.read_csv("student_chatbot/students.csv") 
+    df = pd.read_csv("students.csv") 
     df['Reg. Number'] = df['Reg. Number'].astype(str)
     return df
 
